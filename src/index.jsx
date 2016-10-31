ab466df1e7a3dde37627f6f1cd865c4dcec40ff8
Miguel Alejandro <admin@miguelvinan.com>
2016-10-31 10:30:05 -0500

    Initial setup workflow

---
+++ src/index.jsx
@@ -0,0 +1,15 @@
+import ReactDOM from 'react-dom'
+import React, { Component } from 'react'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello</h1>
+      </div>
+    );
+  }
+}
+
+
+ReactDOM.render(<App />, document.getElementById('root'))
